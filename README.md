563a274d3271c59462b7d083742bcdbb6b7c4ff2
leonlevir <ipleozin@hotmail.com>
2025-10-07 01:30:01 -0500

    new start

---
+++ README.md
@@ -0,0 +1,18 @@
+### HYPRPAPER/WAYPAPER :house:
+<img width="1600" height="900" alt="2025-10-05-223455_hyprshot" src="https://github.com/user-attachments/assets/b33f0557-5ac7-4a69-b1d8-e430a6d979d8" />
+
+### KITTY TERMINAL :computer:
+
+<img width="1600" height="900" alt="2025-10-05-223707_hyprshot" src="https://github.com/user-attachments/assets/983496be-9864-4688-815a-ccc099a95a03" /> 
+
+### ROFI LAUCHER :arrow_forward: (ONE OF DEFAULT THEMES) 
+<img width="1600" height="900" alt="2025-10-05-232434_hyprshot" src="https://github.com/user-attachments/assets/c8c47a96-7aef-4e3d-b1cc-6c6db24c2932" />
+
+### THUNAR EXPLORER :file_folder: (FROM XFCE)
+<img width="1600" height="900" alt="2025-10-05-223732_hyprshot" src="https://github.com/user-attachments/assets/3f901504-5151-4dff-9049-1ecf5920f162" />
+
+## NVIM (MONOKAI PRO) :page_facing_up: / ZSH SHELL 'FISHY' 🐟
+<img width="1600" height="900" alt="2025-10-06-003532_hyprshot" src="https://github.com/user-attachments/assets/ac4769c0-ec42-4370-a074-bf0eb5e6298f" />
+
+## DEFAULT TILING  :triangular_ruler: 'DWINDLE' 
+<img width="1600" height="900" alt="2025-10-05-224323_hyprshot" src="https://github.com/user-attachments/assets/3d138879-63e9-418d-ad53-74bae0a43c15" />
---
+++ mypackages.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#LD_PRELOAD=/usr/\$LIB/libgamemodeauto.so mangohud %command%
+
+# Script de instalação de pacotes para Arch Linux
+
+# Atualizando o sistema antes de iniciar
+echo "Atualizando o sistema..."
+sudo pacman -Syu --noconfirm
+
+# =====================================================================
+# CLASSIFICANDO E INSTALANDO PACOTES ESSENCIAIS E UTILITÁRIOS
+# =====================================================================
+
+echo "Instalando pacotes essenciais..."
+
+# Pacotes de Terminal
+sudo pacman -S --noconfirm nano htop fastfetch curl eza screen
+
+# Ferramentas de Rede
+sudo pacman -S --noconfirm bridge-utils git fwupd zerotier-one
+
+# Virtualização
+sudo pacman -S --noconfirm wine qemu-desktop libvirt virt-manager filezilla docker
+
+# Gaming
+sudo pacman -S --noconfirm steam discord gamemode lib32-gamemode
+
+# Ferramentas Gráficas e Multimídia
+sudo pacman -S --noconfirm obs-studio qbittorrent kdenlive gimp drawing mangohud mpv mousepad loupe
+
+# Ferramentas de Arquivo e Sistema
+sudo pacman -S --noconfirm jre-openjdk udftools ntfs-3g gnome-disk-utility bluez bluez-utils blueman
+
+# Ferramentas de Compactação
+sudo pacman -S --noconfirm zip unzip file-roller 7zip ark
+
+# Instalando o Flatpak via Pacman
+sudo pacman -S --noconfirm flatpak
+sudo flatpak install -y flathub net.pcsx2.PCSX2
+sudo flatpak install -y brave
+
+# Habilitando e Iniciando Serviços com systemctl
+sudo systemctl enable bluetooth.service --now
+sudo systemctl enable zerotier-one.service --now
+sudo systemctl enable docker --now
+
+# Atualiza o sistema
+sudo pacman -Syu --noconfirm
+
+
+# =====================================================================
+# FINALIZAÇÃO
+# =====================================================================
+
+echo "Instalação concluída!"
+

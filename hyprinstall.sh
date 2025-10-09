d42bb7541c7730e135eec009e1f2ae97be7c225f
leonlevir <ipleozin@hotmail.com>
2025-10-08 22:30:44 -0500

    documenting

---
+++ hyprinstall.sh
@@ -0,0 +1,15 @@
+sudo pacman -S hypridle hyprlock hyprpaper nwg-look ngw-displays hyprshot pavucontrol waybar tumbler ffmpegthumbnailer libopenraw poppler-glib gst-libav evince thunar rofi-wayland polkit-gnome gnome-keyring cliphist 
+
+sudo pacman -S -needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -S flat-remix-gtk
+yay -S waypaper
+yay -S brave-bin
+yay -S pcsx2-git
+
+
+
+

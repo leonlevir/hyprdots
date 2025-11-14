65beb2b5e3db1f987692824fb4c8fe6a39252645
leonlevir <ipleozin@hotmail.com>
2025-11-13 22:03:08 -0300

    att

---
+++ install.sh
@@ -0,0 +1,22 @@
+#auth
+polkit-gnome gnome-keyring
+
+#Envs
+sudo pacman -S nwg-look ngw-displays pavucontrol waybar rofi-wayland network-manager-applet cliphist cava
+
+#hypr ecosystem
+sudo pacman -S hypridle hyprlock hyprpaper hyprshot dunst
+
+#thunar
+sudo pacman -S thunar tumbler ffmpegthumbnailer libopenraw poppler-glib gst-libav evince thunar-archive-plugin thunar-volman
+
+#tools
+sudo pacman -S imagemagick
+
+#fonts
+sudo pacman -S ttf-font-awesome ttf-jetbrains-mono-nerd
+
+yay -S flat-remix-gtk waypaper brave-bin pcsx2-git wallust-git
+
+
+
